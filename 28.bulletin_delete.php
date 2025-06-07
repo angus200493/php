e92998f0523809708a56fd17d9dbb312a3d1308c
angus200493 <angus200493@gmail.com>
2025-06-07 14:32:25 +0800

    Update 28.bulletin_delete.php

--- 28.bulletin_delete.php
+++ 28.bulletin_delete.php
@@ -7,13 +7,13 @@
     }
     else{   
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-        $sql="delete from bulletin where bid='{$_GET["bid"]}'";
+        $sql="delete from bulletin where bid='{$_GET["bid"]}'";#根據URL參數刪除指定bid的佈告
         #echo $sql;
         if (!mysqli_query($conn,$sql)){
-            echo "佈告刪除錯誤";
+            echo "佈告刪除錯誤";#執行失敗顯示佈告刪除錯誤
         }else{
-            echo "佈告刪除成功";
+            echo "佈告刪除成功";#成功顯示刪除成功
         }
-        echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
+        echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";#無論成功或失敗三秒後跳至11.bulletin.php
     }
 ?>
