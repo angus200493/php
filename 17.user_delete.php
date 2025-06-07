cb6b7fc7e7fe196d9542f8db3c7b89e0a42072fb
angus200493 <angus200493@gmail.com>
2025-06-07 13:40:55 +0800

    Update 17.user_delete.php

--- 17.user_delete.php
+++ 17.user_delete.php
@@ -7,13 +7,13 @@
     }
     else{   
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-        $sql="delete from user where id='{$_GET["id"]}'";
+        $sql="delete from user where id='{$_GET["id"]}'";#從user表格中刪除指定id使用者，id從URL參數取得
         #echo $sql;
         if (!mysqli_query($conn,$sql)){
-            echo "使用者刪除錯誤";
+            echo "使用者刪除錯誤";#執行失敗
         }else{
-            echo "使用者刪除成功";
+            echo "使用者刪除成功";#執行成功
         }
-        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
+        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";#無論失敗成功三秒後跳至18.user.php
     }
 ?>
