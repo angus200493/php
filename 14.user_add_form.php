10ac42a68924f8bfd1e14b11af50fd05cdb1a56c
angus200493 <angus200493@gmail.com>
2025-06-07 13:29:44 +0800

    Update 14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -2,13 +2,13 @@
     <head><title>新增使用者</title></head>
     <body>
 <?php        
-    error_reporting(0);
-    session_start();
+    error_reporting(0);#隱藏錯誤訊息
+    session_start();#檢查使用者是否登入
     if (!$_SESSION["id"]) {
-        echo "請登入帳號";
-        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+        echo "請登入帳號";#若未登入顯示請登入
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";#3秒後跳至2.login.html
     }
-    else{    
+    else{    #若已登入表單提交至 15.user_add.php，包含「帳號」和「密碼」輸入欄。有「新增」（提交）和「清除」（重置）按鈕。
         echo "
             <form action=15.user_add.php method=post>
                 帳號：<input type=text name=id><br>
