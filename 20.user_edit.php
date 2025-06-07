823048b693c0ae83672e6064af47ccc055e255a5
angus200493 <angus200493@gmail.com>
2025-06-07 14:01:00 +0800

    Update 20.user_edit.php

--- 20.user_edit.php
+++ 20.user_edit.php
@@ -9,11 +9,11 @@
     else{   
         $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
         if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){
-            echo "修改錯誤";
-            echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
+            echo "修改錯誤";#若SQL執行失敗，顯示修改錯誤
+            echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";#三秒後跳轉18.user.php
         }else{
-            echo "修改成功，三秒鐘後回到網頁";
-            echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
+            echo "修改成功，三秒鐘後回到網頁";#若SQL執行成功顯示三秒鐘後回到網頁
+            echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";#三秒後跳轉18.user.php
         }
     }
 
